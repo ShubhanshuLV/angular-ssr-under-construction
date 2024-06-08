6394cd102b8a74cd657fb517ef2e4a6b2f1f7770
Jhonatan Arenas <jhonatan.arenas.salazar@gmail.com>
2024-06-08 00:26:14 -0400

    feature: Adding source code

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:22-alpine as build
+WORKDIR /app
+COPY package*.json ./
+RUN npm ci --legacy-peer-deps
+COPY . ./
+RUN npm run build --omit=dev
+
+FROM node:22-alpine
+WORKDIR /app
+COPY --from=build /app/dist ./dist
+COPY --from=build /app/package*.json ./
+ENV NODE_ENV production
+ENV PORT 80
+EXPOSE 80
+CMD ["npm", "start"]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'under-construction';
+export class AppComponent implements OnInit {
+  title = 'Codengine';
+
+  constructor(private metaTagService: Meta) {}
+
+  ngOnInit(): void {
+    this.metaTagService.addTags([{name: 'robots', content: 'noindex, nofollow'}]);
+  }
 }
